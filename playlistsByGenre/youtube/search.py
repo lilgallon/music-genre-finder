33933ece5febb2f9bec28f74e28af6eee2d19553
nero <swellchaser33@gmail.com>
2019-06-23 13:54:24 +0200

    removed header from search.py

--- playlistsByGenre/youtube/search.py
+++ playlistsByGenre/youtube/search.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
 
-# This sample executes a search request for the specified search term.
-# Sample usage:
-#   python search.py --q=surfing --max-results=10
-# NOTE: To use the sample, you must provide a developer key obtained
-#       in the Google APIs Console. Search for "REPLACE_ME" in this code
-#       to find the correct place to provide that key..
-
 from googleapiclient.discovery import build
 
 YOUTUBE_API_SERVICE_NAME = 'youtube'
