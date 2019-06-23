c3b01a90f15daef020513738db3aaa6bf5f6419d
nero <swellchaser33@gmail.com>
2019-06-23 15:59:05 +0200

    updated "key" to "youtube_key" (we could use multiple API in the future)

--- playlistsByGenre/playlistsByGenre.py
+++ playlistsByGenre/playlistsByGenre.py
@@ -16,7 +16,7 @@ def main():
     # ...
     video_ids = []
     for file in files:
-        results = youtube_search(config["API"]["key"], file, 1)
+        results = youtube_search(config["API"]["youtube_key"], file, 1)
         videos = results[0]
         video = videos[0]
         video_id = video[1]
@@ -26,7 +26,7 @@ def main():
     # ...
     playlists = {}
     for video_id in video_ids:
-        genre = find_genre(config["API"]["key"], video_id)
+        genre = find_genre(config["API"]["youtube_key"], video_id)
         if genre in playlists:  # if the genre already exist, we just need to append the video id
             playlists[genre].append(video_id)
         else:  # otherwise, we need to create a new key in the playlists dict
