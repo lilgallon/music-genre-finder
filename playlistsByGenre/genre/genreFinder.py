9ee2a23cba16263e9afae3e598a25e39f7f47a3b
nero <swellchaser33@gmail.com>
2019-07-24 20:38:37 +0200

    basic implementation of genre finder

--- playlistsByGenre/genre/genreFinder.py
+++ playlistsByGenre/genre/genreFinder.py
@@ -1,11 +1,36 @@
 import json
 
 def find_genre(developer_key: str, videos: str) -> str:
-    with open("genre/genres.json") as f:
+    with open("genre/genres.json", "r") as f:
         genres = json.load(f)
 
     # TODO: for the moment, we will only guess the genre by looking at the
     # channel ID. In the future we could look at the description and the tags
     # for example.
 
-    return 'unknown genre'
+    potential_genres = {}
+    for genre in genres.items():
+        genre_name = genre[0]
+        channels = genre[1]
+        potential_genres[genre_name] = 0
+
+        for channel in channels:
+            channel_id = channel["id"]
+            channel_accuracy = channel["accuracy"]
+
+            for video in videos:
+                video_channel = video[1]
+
+                if channel_id == video_channel:
+                    potential_genres[genre_name] += channel_accuracy
+
+    more_accurate_genre = "unknown genre"
+    best_accuracy = 0
+    for genre in potential_genres.items():
+        genre_name = genre[0]
+        accuracy = genre[1]
+        if accuracy > best_accuracy:
+            accuracy = best_accuracy
+            more_accurate_genre = genre[0]
+
+    return more_accurate_genre
